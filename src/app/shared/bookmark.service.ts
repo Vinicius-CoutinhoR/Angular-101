854337c32036e211f6bb87c65651d6e8ba831431
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2024-02-02 09:24:01 -0300

    Added new bookmark model and service

---
+++ src/app/shared/bookmark.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {Bookmark} from "./bookmark.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookmarkService {
+
+  bookmarks: Bookmark[] = [];
+
+  constructor() { }
+
+  getBookmarks() {
+    return this.bookmarks
+  }
+
+  getBookmark(id: string) {
+    return this.bookmarks.find(b => b.id === id)!;
+  }
+
+  addBookmark(bookmark: Bookmark) {
+    this.bookmarks.push(bookmark);
+  }
+
+  updateBookmark(id: string, updatedFields: Partial<Bookmark>) {
+    const bookmark = this.getBookmark(id);
+    Object.assign(bookmark, updatedFields);
+  }
+
+  deleteBookmark(id: string): void {
+    const bookmarkIndex = this.bookmarks.findIndex(b => b.id === id)!;
+    if (bookmarkIndex !== -1) {
+      this.bookmarks.splice(bookmarkIndex, 1);
+    }
+  }
+
+}
