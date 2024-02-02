c16d781b9e323aa31a8c6efaf37848db2ac37157
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2024-02-02 09:23:04 -0300

    Updated todos component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,18 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {TodoService} from "../shared/todo.service";
 import {Todo} from "../shared/todo.model";
+import {Router} from "@angular/router";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.scss'
+  styleUrl: './todos.component.scss',
+  animations: [
+    trigger('todoItemAnim', [
+      transition(':leave', [
+        animate(200, style({
+          opacity: 0,
+          height: 0,
+          marginBottom: 0,
+        }))
+      ])
+    ])
+  ]
 })
 export class TodosComponent implements OnInit {
 
   todos: Todo[] = [];
 
   constructor(
-    private todoService: TodoService
+    private todoService: TodoService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -23,6 +37,14 @@ export class TodosComponent implements OnInit {
     this.todoService.updateTodo(todo.id, {
       completed: !todo.completed
     })
+  }
+
+  onEditClick(todo: Todo) {
+    this.router.navigate(['/todos', todo.id])
+  }
+
+  onDeleteClick(todo: Todo) {
+    this.todoService.deleteTodo(todo.id);
   }
 
 }
