0696d2e990c1cc7de058897f28db5f8bfbadafa4
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2024-02-02 09:22:39 -0300

    Updated app module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import {NotesComponent} from "./notes/notes.component";
 import {AddNoteComponent} from "./add-note/add-note.component";
 import {EditNoteComponent} from "./edit-note/edit-note.component";
 import {AddTodoComponent} from "./add-todo/add-todo.component";
+import {EditTodoComponent} from "./edit-todo/edit-todo.component";
 
 const routes: Routes = [
   { path: 'bookmarks', component: BookmarksComponent, data: { tab: 0 }},
   { path: 'todos', component: TodosComponent, data: { tab: 1 }},
-  { path: 'todos/add', component: AddTodoComponent},
+  { path: 'todos/add', component: AddTodoComponent },
+  { path: 'todos/:id', component: EditTodoComponent },
   { path: 'notes', component: NotesComponent, data: { tab: 2 }},
   { path: 'notes/add', component: AddNoteComponent },
   { path: 'notes/:id', component: EditNoteComponent },
