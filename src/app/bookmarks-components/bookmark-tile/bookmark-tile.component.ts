cdda7882973b935fda4b6b0e1077ecf5715ac397
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2024-02-06 11:51:55 -0300

    Updated bookmark-tile.component

---
+++ src/app/bookmarks-components/bookmark-tile/bookmark-tile.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Bookmark} from "../../shared/bookmark.model";
+
+@Component({
+  selector: 'app-bookmark-tile',
+  templateUrl: './bookmark-tile.component.html',
+  styleUrl: './bookmark-tile.component.scss'
+})
+export class BookmarkTileComponent implements OnInit {
+
+  @Input() bookmark: Bookmark = new Bookmark('Example', 'https://angular.io/');
+
+  tileIconSrc?: string;
+
+  faviconError: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.tileIconSrc = this.bookmark?.url.origin + '/favicon.ico'
+  }
+
+}
