e00e84d4d6c9de181f66a4c87613be6d4f18e281
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2024-02-06 11:50:42 -0300

    Created add-bookmark.component

---
+++ src/app/bookmarks-components/add-bookmark/add-bookmark.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {BookmarkService} from "../../shared/bookmark.service";
+import {Bookmark} from "../../shared/bookmark.model";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-add-bookmark',
+  templateUrl: './add-bookmark.component.html',
+  styleUrl: './add-bookmark.component.scss'
+})
+export class AddBookmarkComponent implements OnInit {
+
+  constructor(
+    private bookmarkService: BookmarkService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onFormSubmit(form: NgForm) {
+    const { name, url } = form.value;
+    const bookmark: Bookmark = new Bookmark(name, url);
+    this.bookmarkService.addBookmark(bookmark);
+    this.router.navigateByUrl("/bookmarks")
+  }
+}
