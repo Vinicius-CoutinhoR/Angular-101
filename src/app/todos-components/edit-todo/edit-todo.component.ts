34add0ff5d211622852e0b6fae6692a87e1377db
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2024-02-07 10:54:58 -0300

    Updated Added edit-todo component

---
+++ src/app/todos-components/edit-todo/edit-todo.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap, Route, Router} from "@angular/router";
+import {TodoService} from "../../shared/service/todo.service";
+import {Todo} from "../../shared/model/todo.model";
+import {NgForm} from "@angular/forms";
+import {Note} from "../../shared/model/note.model";
+
+@Component({
+  selector: 'app-edit-todo',
+  templateUrl: './edit-todo.component.html',
+  styleUrl: './edit-todo.component.scss'
+})
+export class EditTodoComponent implements OnInit {
+
+  todo: Todo = new Todo('');
+
+  showValidationErrors: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private todoService: TodoService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const todoId: any = paramMap.get('id') ?? '';
+      this.todo = this.todoService.getTodo(todoId);
+    })
+  }
+
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) {
+      return
+    }
+    this.todoService.updateTodo(this.todo.id, form.value);
+    this.router.navigateByUrl("/todos");
+  }
+}
